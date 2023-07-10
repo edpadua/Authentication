590654de45e365030cb099bfdbd776e8c9fbda30
edpadua <ed.padua@gmail.com>
2023-07-10 18:17:09 -0300

    More configurations
    More configurations and the first components

---
+++ authentication/src/Components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import tw from "tailwind-styled-components"
+
+const Nav = tw.nav`
+    bg-cyan-500
+    top-0 
+    left-0 
+    right-0 
+    w-full 
+    h-20 
+    pt-5
+`;
+
+function Navbar() {
+    return (
+        <Nav>
+
+        </Nav>
+    )
+}
+
+export default Navbar
